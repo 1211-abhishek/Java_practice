eb615eb46741fe4c3757142b6087f034feea2050
1211-abhishek <abhishekghogare.eidiko@gmail.com>
2025-08-25 18:52:18 +0530

    Email sender service

--- email_service/src/main/java/com/example/email_service/controller/EmailSenderController.java
+++ email_service/src/main/java/com/example/email_service/controller/EmailSenderController.java
@@ -1,4 +1,28 @@
 package com.example.email_service.controller;
 
+import com.example.email_service.service.EmailSenderService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
 public class EmailSenderController {
+
+    @Autowired
+    EmailSenderService emailSenderService;
+
+    @PostMapping("/send-html")
+    public String sendMail(@RequestBody String message){
+
+        try {
+            return emailSenderService.sendMimeMail(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- email_service/src/main/java/com/example/email_service/service/EmailSenderService.java
+++ email_service/src/main/java/com/example/email_service/service/EmailSenderService.java
@@ -1,4 +1,68 @@
 package com.example.email_service.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+@Service
 public class EmailSenderService {
+
+    int i = 0;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");
+
+    // @Scheduled(fixedDelay = 10000)
+    public void sendSimpleMailAtFixedRate() {
+
+        System.out.println("sending mail : " + LocalDateTime.now().format(formatter));
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo("abhishekghogare.java@gmail.com");
+        simpleMailMessage.setSubject("mail test : " + i);
+//        simpleMailMessage.setText("This is test mail " + i++);
+        simpleMailMessage.setText("This is test mail " + i++ + System.lineSeparator() + "Date time : " + LocalDateTime.now());
+        System.out.println(simpleMailMessage);
+        javaMailSender.send(simpleMailMessage);
+
+        System.out.println("mail sent : " + LocalDateTime.now().format(formatter));
+
+    }
+
+    @Async
+    public String sendMimeMail(String message) throws MessagingException {
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setTo("abhishekghogare.java@gmail.com");
+        mimeMessageHelper.setSubject("Test Mime message : " + i);
+
+        mimeMessageHelper.setText(message, true);
+        mimeMessageHelper.addAttachment("report.pdf", new File("src/main/resources/reports/report.pdf"));
+
+        ClassPathResource classPathResource = new ClassPathResource("doc/aadhar8.jpeg");
+
+        String htmlImage = "<img src='cid:image'/>";
+        mimeMessageHelper.setText(htmlImage, true);
+        mimeMessageHelper.addInline("image", classPathResource);
+
+        javaMailSender.send(mimeMessage);
+
+        return "Success";
+    }
 }
